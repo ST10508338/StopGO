8f86e96b4b9c671e03ca90a3ebaebdea7464af82
ST10508338 <ST10508338@Myemeris.edu.za>
2026-02-25 14:42:06 +0200

    Final submission

---
+++ src/stopngo/Stopngo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopngo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Stopngo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("                     ----------                    ");
+     System.out.println("        ********                   XXXXXXXX        ");
+     System.out.println("       *        *                 X       X        ");
+     System.out.println("      *  START   *               X  STOP   X       ");
+     System.out.println("       *        *                 X       X        ");
+     System.out.println("        ********                   XXXXXXX         ");
+     System.out.println("           **                         XX            ");
+     System.out.println("           **                         XX            ");
+     System.out.println("           **                         XX            ");
+     System.out.println("           **                         XX            ");
+     System.out.println("          ****                       XXXX           ");
+     System.out.println("         ******                     XXXXXX          ");
+ 
+}
+    
+ 
+    
+}
